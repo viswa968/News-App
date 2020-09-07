0ada7c07302312d90c59c914e19eea898eee5d02
viswa968 <viswa968@gmail.com>
2020-09-07 13:39:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+
+
+
+import React, {Component} from 'react';
+
+import TabScreen from './src/screens/TabScreen';
+import { Tab, Tabs } from 'native-base';
+
+
+export default class App extends Component{
+  render() {
+    return (
+      <TabScreen/>
+    )
+  }
+}
